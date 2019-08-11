cc6fc71fce0ff28818189a036d52051c2c60d16a
Dr.McHack <dr.mchack@gmail.com>
2019-08-11 16:51:01 +0200

    Change serverProperties to class

--- nbukkit/server/serverProperties.js
+++ nbukkit/server/serverProperties.js
@@ -1,19 +1,15 @@
-module.exports = {
-    "gamemode": 0,
-    "difficulty": 0,
-    "spawnMonsters": true,
-    "pvp": true,
-    "levelType": "DEFAULT",
-    "enableCommandBlock": true,
-    "serverPort": 25565,
-    "serverIp": "localhost",
-    "spawnNpcs": false,
-    "allowFlight": false,
-    "levelName": "world",
-    "whitelist": false,
-    "onlineMode": true,
-    "maxBuildHeight": 256,
-    "levelSeed": "",
-    "motd": "NBukkit Minecraft Server",
-    "version": "1.8.8"
+const fs = require("fs");
+
+module.exports = class Properties{
+    constructor(){
+        this.data = {};
+        this.path = "";
+    }
+    load(path){
+        this.path = path;
+        if(this.path){
+            this.data = fs.readFileSync(path);
+        }
+        return this.data;
+    }
 }
