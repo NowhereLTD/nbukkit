95562f75244b8d59b9c06381b68ffa26aa87865d
Dr.McHack <dr.mchack@gmail.com>
2019-08-11 16:07:48 +0200

    Optimize Server Properties

--- nbukkit/server/serverProperties.js
+++ nbukkit/server/serverProperties.js
@@ -1,3 +1,18 @@
 module.exports = {
-    
+    "gamemode": 0,
+    "difficulty": 0,
+    "spawnMonsters": true,
+    "pvp": true,
+    "levelType": "DEFAULT",
+    "enableCommandBlock": true,
+    "serverPort": 25565,
+    "serverIp": "localhost",
+    "spawnNpcs": false,
+    "allowFlight": false,
+    "levelName": "world",
+    "whitelist": false,
+    "onlineMode": true,
+    "maxBuildHeight": 256,
+    "levelSeed": "",
+    "motd": "NBukkit Minecraft Server"
 }
